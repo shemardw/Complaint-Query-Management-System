07e834a3a9bce76966b9b32d3d44c991cadb7fef
shemardw <57420118+shemardw@users.noreply.github.com>
2023-04-10 23:13:14 -0500

    Update LoggingTest.java

--- APGroup_Utech_Complaint_System/src/testing/LoggingTest.java
+++ APGroup_Utech_Complaint_System/src/testing/LoggingTest.java
@@ -1,4 +1,4 @@
-package testing;
+/*package testing;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -20,4 +20,47 @@ public class LoggingTest {
 		demologger.debug("This is debug");
 	}
 
+}*/
+
+package testing; 
+
+import java.util.logging.FileHandler; 
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+import java.util.logging.SimpleFormatter; 
+
+public class LoggingTest {
+	private static Logger Logger = Logger.getLogger("My Log"); 
+	public static void init(){
+		FileHandler fh; 
+		try{
+			//fh = new FileHandler("C:/MyLogFile.log",true); 
+			fh = new FileHandler(System.getProperty("user.dir")+System.getProperty("file separator")+"MyLogFile.log", true); //to append log files 
+			Logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter(); 
+			fh.setFormatter(formatter); 
+
+			Logger.info("Logger Initialized."); 
+		} catch (Exception e){
+			Logger.log(Level.WARNING,"Exception ::", e);
+		}
+	}
+
+public static void writeToLog(String msg){
+	Logger.info(msg);
 }
+public static void writeToLog(String msg, Exception exp){
+	Logger.log(Level.WARNING,msg,exp);
+}
+
+public static void main(String[]args){
+	init(); 
+	try{
+		int a = 10/0; //arithmetic exception 
+	} 
+	catch (Exception e){
+		Logger.log(Level.WARNING, "Exception ::", e); 
+	}
+	Logger.info("End of Program."); 
+
+
